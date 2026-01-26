7aca39bbbb85abb7bf07bbd278dd914e9eb308b5
vishwajeet161 <vishwajeet.patel161@gmail.com>
2026-01-26 18:13:06 +0530

    adding analysis and document services

---
+++ backend/document-service/src/test/java/com/legaldocinsight/document_service/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legaldocinsight.document_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
