b05453b7a7085b2f3812bc56513745b29b092667
vishwajeet161 <vishwajeet.patel161@gmail.com>
2026-01-26 19:50:12 +0530

    all services are working fine now, next will add logic

---
+++ backend/document-service/src/main/java/com/legaldocinsight/document_service/HealthController.java
@@ -0,0 +1,13 @@
+package com.legaldocinsight.document_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Document Service is UP";
+    }
+}
